96964e83d8511a1269f24940480f1d68accd8ca6
Christopher Yahir <223209@ids.upchiapas.edu.mx>
2024-02-29 11:23:15 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import { Server } from 'socket.io'
+import cors from 'cors'
+import dotenv from 'dotenv';
+dotenv.config()
+
+const app = express()
+
+const port = 3002
+
+app.use(express.json())
+app.use(cors());
+
+const server = app.listen(port, () => {
+
+      console.log('api-websocket is running on port 3002')
+
+})
+
+const io = new Server(server, {
+      cors : {
+            origin : process.env.CLIENTE,
+            methods : ['POST', 'GET']
+      }
+});
+
+io.on('connection', socket => {
+
+      console.log('User connected')
+
+      socket.on('Task created', task => {
+            console.log('task received: ', task)      
+            io.emit('task-user', task)
+      })
+
+      socket.on('disconnect', () => {
+            console.log('User disconnected')
+      })
+})
